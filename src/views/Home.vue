9915405391ca9c46d3b6ddeb757c31dae5e01e63
Accel <accelwtf@gmail.com>
2019-06-20 12:49:33 +0700

    Add display and clear info functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,23 +7,35 @@
       </ion-header>
       <ion-content class="ion-padding">
          <ZipSearch v-on:get-zip="getZipInfo" />
+         <ZipInfo v-bind:info="info" />
+         <ClearInfo v-bind:info="info" v-on:clear-info="clearInfo" />
       </ion-content>
    </div>
 </template>
 
 <script>
 import ZipSearch from "../components/ZipSearch";
+import ZipInfo from "../components/ZipInfo";
+import ClearInfo from "../components/ClearInfo";
 export default {
    name: "home",
-   components: { ZipSearch },
+   components: { ZipSearch, ZipInfo, ClearInfo },
+   data() {
+      return {
+         info: null
+      };
+   },
    methods: {
       async getZipInfo(zip) {
          const res = await fetch(`https://api.zippopotam.us/us/${zip}`);
          if (res.status == 404) {
             this.showAlert();
          }
-         const info = await res.json();
-         console.log(info);
+         this.info = await res.json();
+         console.log(this.info);
+      },
+      clearInfo() {
+         this.info = null;
       },
       showAlert() {
          return this.$ionic.alertController
